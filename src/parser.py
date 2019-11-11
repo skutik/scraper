ade7ab629757a2236c20880d534b0fb604ad578a
Dave <david.szkuta@email.cz>
2019-11-11 22:02:47 +0100

    update test and parser class
    update tests and parser class

--- src/parser.py
+++ src/parser.py
@@ -13,7 +13,7 @@ class Parser:
     def get_dict(self):
         page = self.page
         item = dict()
-        if page.find("div", class_="params clear").findAll("ul"):
+        if page.find("div", class_="params clear"):
             for ul in page.find("div", class_="params clear").findAll("ul"):
                 for li in ul.findAll("li"): 
                     label = li.find("label").text[:-1]
@@ -29,17 +29,17 @@ class Parser:
                             value.append(unicodedata.normalize("NFKD",span.text))
                     if isinstance(value, list):
                         item[label] = " ".join(value)
-                    else:
-                        item[label] = value
-            item["adress"] = page.find("span", class_="location-text ng-binding").text
+                    item[label] = value[0]
+            item["adresa"] = page.find("span", class_="location-text ng-binding").text
             price_string = unicodedata.normalize("NFKD",page.find("span", class_="norm-price ng-binding").text)
-            price = re.findall(r"\d+", price_string)
-            item["cena"] = "".join(price)
+            price = re.findall(r"\d+", price_string) # Convert page price s string to integer
+            item["cena"] = int("".join(price))
             item["url"] = self.page_url
-            item = {unidecode.unidecode(key).lower().replace(" ","_"): value  for key, value in item.items()}
+            # Change accents in keys to english letters and replace spaces e.g. "Celková cena" to "celkova_cena"
+            item = {unidecode.unidecode(key).lower().replace(" ","_"): value  for key, value in item.items()} 
             return item
         else: 
-            return item
+            return None
 
 
 
--- tests/parser_test.py
+++ tests/parser_test.py
@@ -6,12 +6,20 @@ class ParserTest(unittest.TestCase):
     with open("tests/pages/advertisement_page.html") as html_file:
         html = html_file.read()
 
-    parser = Parser(html, "www.testpage.com")
+    testParser = Parser(html, "www.testpage.com")
+    emptyParser = Parser("", "")
 
-    def test_parser(self, parser):
-        advert_dict = parser.get_dict()
+    def test_parser(self):
+        advert_dict = self.testParser.get_dict()
+        emptyPage = self.emptyParser.get_dict()
+        print(advert_dict)
         self.assertEqual(len(advert_dict), 15)
+        self.assertEqual(type(advert_dict["cena"]), int)
+        self.assertEqual(advert_dict["poznamka_k_cene"], "Služby 1000 Kč + provize RK")
         self.assertEqual(advert_dict["url"],  "www.testpage.com")
+        self.assertEqual(emptyPage,None)
+        with self.assertRaises(TypeError):
+            emptyPage["url"]
 
 if __name__ == "__main__":
     unittest.main()
